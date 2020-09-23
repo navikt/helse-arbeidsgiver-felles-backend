af582262bf00ab7a2dd7033f3f76b571280589c8
tuxbear <ole-andre.riga.johansen@nav.no>
2020-09-23 22:19:44 +0200

    PDL Klient

--- src/main/kotlin/no/nav/helse/arbeidsgiver/integrasjoner/pdl/PdlClient.kt
+++ src/main/kotlin/no/nav/helse/arbeidsgiver/integrasjoner/pdl/PdlClient.kt
@@ -9,55 +9,45 @@ import kotlinx.coroutines.runBlocking
 import no.nav.helse.arbeidsgiver.integrasjoner.RestStsClient
 import org.slf4j.LoggerFactory
 
-class PdlClient(
+interface PdlClient {
+    fun person(ident: String): PdlPerson?
+}
+
+class PdlClientImpl(
         private val pdlUrl: String,
         private val stsClient: RestStsClient,
         private val httpClient: HttpClient,
         private val om: ObjectMapper
-) {
+) : PdlClient {
     private val query = this::class.java.getResource("/pdl/hentPerson.graphql").readText().replace(Regex("[\n\r]"), "")
 
     init {
         LOG.debug("Query: $query")
     }
 
-    fun person(ident: String): PdlHentPerson? {
+    override fun person(ident: String): PdlPerson? {
         val stsToken = stsClient.getOidcToken()
         val entity = PdlRequest(query, Variables(ident))
-        try {
-            val pdlPersonReponse = runBlocking {
-                 httpClient.post<PdlPersonResponse> {
-                    url(pdlUrl)
-                    body = TextContent(om.writeValueAsString(entity), contentType = ContentType.Application.Json)
-                    header("Tema", "SYK")
-                    header("Authorization", "Bearer $stsToken")
-                    header("Nav-Consumer-Token", "Bearer $stsToken")
-                }
+        val pdlPersonReponse = runBlocking {
+            httpClient.post<PdlPersonResponse> {
+                url(pdlUrl)
+                body = TextContent(om.writeValueAsString(entity), contentType = ContentType.Application.Json)
+                header("Tema", "SYK")
+                header("Authorization", "Bearer $stsToken")
+                header("Nav-Consumer-Token", "Bearer $stsToken")
             }
-
-            return if (pdlPersonReponse.errors != null && pdlPersonReponse.errors.isNotEmpty()) {
-                pdlPersonReponse.errors.forEach {
-                    LOG.error("Error while requesting person from PersonDataLosningen: ${it.errorMessage()}")
-                }
-                null
-            } else {
-                pdlPersonReponse.data
-            }
-        } catch (exception: Exception) {
-            LOG.error("Error from PDL with request-url: $pdlUrl", exception)
-            throw exception
         }
-    }
 
-    fun personName(ident: String): String? {
-        return person(ident)?.fullName()
-    }
+        if (pdlPersonReponse.errors != null && pdlPersonReponse.errors.isNotEmpty()) {
+            throw PdlException(pdlPersonReponse.errors)
+        }
 
-    fun isKode6Or7(ident: String): Boolean {
-        return person(ident)?.isKode6Or7() ?: true
+        return pdlPersonReponse.data?.hentPerson
     }
 
     companion object {
         private val LOG = LoggerFactory.getLogger(PdlClient::class.java)
     }
+
+    class PdlException(val pdlErrors: List<PdlError>?) : Exception()
 }
