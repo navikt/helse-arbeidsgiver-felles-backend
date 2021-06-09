e05c9568d90d32f4d856af7e662f06ca8564f0ba
tuxbear <ole-andre.riga.johansen@nav.no>
2021-06-09 15:22:58 +0300

    POH-751: Eget interface og implementasjon for PDL-spørringer med innlogget bruker

--- src/main/kotlin/no/nav/helse/arbeidsgiver/integrasjoner/pdl/PdlClient.kt
+++ src/main/kotlin/no/nav/helse/arbeidsgiver/integrasjoner/pdl/PdlClient.kt
@@ -18,6 +18,16 @@ interface PdlClient {
 }
 
 /**
+ * Interface for en PDL-klient som har en innlogget saksbehandler.
+ * Når denne brukes gjør PDL en sjekk av at brukeren har tilgang til å se informasjon om identen
+ * det spørres om
+ */
+interface LoggedInPdlClient {
+    fun personNavn(ident: String, userLoginToken: String): PdlHentPersonNavn.PdlPersonNavneliste?
+    fun fullPerson(ident: String, userLoginToken: String): PdlHentFullPerson?
+}
+
+/**
  * Enkel GraphQL-klient for PDL som kan enten hente navn fra aktør eller fnr (ident)
  * eller hente mer fullstendig data om en person via fnr eller aktørid (ident)
  *
@@ -32,7 +42,7 @@ class PdlClientImpl(
         private val stsClient: AccessTokenProvider,
         private val httpClient: HttpClient,
         private val om: ObjectMapper
-) : PdlClient {
+) : PdlClient, LoggedInPdlClient {
     private val personNavnQuery = this::class.java.getResource("/pdl/hentPersonNavn.graphql").readText().replace(Regex("[\n\r]"), "")
     private val fullPersonQuery = this::class.java.getResource("/pdl/hentFullPerson.graphql").readText().replace(Regex("[\n\r]"), "")
 
@@ -49,15 +59,26 @@ class PdlClientImpl(
         return response
     }
 
+    override fun personNavn(ident: String, userLoginToken: String): PdlHentPersonNavn.PdlPersonNavneliste? {
+        val entity = PdlQueryObject(personNavnQuery, Variables(ident))
+        val response = queryPdl<PdlHentPersonNavn?, PdlResponse<PdlHentPersonNavn?>>(entity, userLoginToken)
+        return response?.hentPerson
+    }
 
-    private inline fun <K, reified T: PdlResponse<K>> queryPdl(graphqlQuery: PdlQueryObject): K? {
+    override fun fullPerson(ident: String, userLoginToken: String): PdlHentFullPerson? {
+        val queryObject = PdlQueryObject(fullPersonQuery, Variables(ident))
+        val response = queryPdl<PdlHentFullPerson?, PdlResponse<PdlHentFullPerson?>>(queryObject, userLoginToken)
+        return response
+    }
+
+    private inline fun <K, reified T: PdlResponse<K>> queryPdl(graphqlQuery: PdlQueryObject, loggedInUserToken: String? = null): K? {
         val stsToken = stsClient.getToken()
         val pdlPersonReponse = runBlocking {
             httpClient.post<T> {
                 url(pdlUrl)
                 body = TextContent(om.writeValueAsString(graphqlQuery), contentType = ContentType.Application.Json)
                 header("Tema", "SYK")
-                header("Authorization", "Bearer $stsToken")
+                header("Authorization", "Bearer ${loggedInUserToken?: stsToken}")
                 header("Nav-Consumer-Token", "Bearer $stsToken")
             }
         }
