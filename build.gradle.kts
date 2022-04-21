cdc42c21753475d5cc3c885be690795f8f727ebe
Morten Laukvik <morten.laukvik@nav.no>
2022-04-21 13:59:20 +0200

    Jacoco blir alltid kjørt på test

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,9 +67,6 @@ tasks.jacocoTestCoverageVerification {
 tasks.test {
     finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
 }
-tasks.jacocoTestReport {
-    dependsOn(tasks.test) // tests are required to run before generating the report
-}
 
 buildscript {
     dependencies {
