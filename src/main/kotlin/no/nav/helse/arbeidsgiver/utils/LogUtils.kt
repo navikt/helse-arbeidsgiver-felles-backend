65af9ee67a0450286c23af098e19f4e6d2698d3b
Mikael Bjerga <mikael.bjerga@nav.no>
2022-07-04 14:46:07 +0200

    Gjør loggeren til superklasse RecurringJob private

---
+++ src/main/kotlin/no/nav/helse/arbeidsgiver/utils/LogUtils.kt
@@ -0,0 +1,7 @@
+package no.nav.helse.arbeidsgiver.utils
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+internal fun <T : Any> T.logger(): Logger =
+    LoggerFactory.getLogger(this.javaClass)
